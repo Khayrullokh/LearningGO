38f1e2be0f966ffb8952b68cf61e50af255a2f3b
Khayrullokh <142790323+Khayrullokh@users.noreply.github.com>
2023-08-24 03:34:19 -0700

    fixed

--- main.go
+++ main.go
@@ -7,19 +7,25 @@ import (
 
 func main() {
 	var a float64 = 1.0
-	var b float64 = -1.0
-	var c float64 = 4.0
+	var b float64 = 2.0
+	var c float64 = 1.0
 	// ax^2 + bx + c
 
-	var d float64 = b * b - 4.0 * a * c
+	var d float64 = b*b - 4.0*a*c
 	fmt.Printf("d = %f\n", d)
-	var sqrt_d float64 = math.Sqrt(d)
-	fmt.Printf("sqrt_d = %f\n", sqrt_d)
-	
-	var x1 float64 = (-b + sqrt_d) / (2 * a)
-	fmt.Printf("x1 = %f\n", x1)
-	var x2 float64 = (-b - sqrt_d) / (2 * a)
-	fmt.Printf("x2 = %f\n", x2)
-	
-	fmt.Printf("x1 = %f, x2 = %f;\n", x1, x2)
+
+	if d > 0 {
+		fmt.Println("There are 2 answers")
+		var sqrt_d float64 = math.Sqrt(d)
+		fmt.Printf("sqrt_d = %f\n", sqrt_d)
+		var x1 float64 = (-b + sqrt_d) / (2 * a)
+		var x2 float64 = (-b - sqrt_d) / (2 * a)
+		fmt.Printf("x1 = %f, x2 = %f;\n", x1, x2)
+	} else if d == 0 {
+		fmt.Println("There is only one answer")
+		x := (-b) / (2 * a)
+		fmt.Printf("x = %f;\n", x)
+	} else {
+		fmt.Println("There is not an answer")
+	}
 }
