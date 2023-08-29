3d0fbba91c04f89105d2994390347666a7ba0f98
Khayrullokh <142790323+Khayrullokh@users.noreply.github.com>
2023-08-29 12:50:54 +0500

    dunno

--- main.go
+++ main.go
@@ -7,14 +7,19 @@ func main() {
 	fmt.Scanln(&a)
 	fmt.Print("-------------------------------\n")
     
-	var i int = 1
+	var i int = 0
 	for i <= a {
-		var b int = 1
+		i = i + 1
+		var b int = 0
 		for b <= a {
-			fmt.Printf("* ")
-			b = b + 1
+			b = b + 1 
+			if i == 0 || i == a-1 || b == 0 || b == a-1 {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
 		} 
 		fmt.Println()
-		i = i + 1
+		
+		}
 	}
 }
